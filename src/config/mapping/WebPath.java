dfdc72981b3122fa8ca48f8b68a5c72c75481ba6
carlospicest <cpe17494@gmail.com>
2022-10-24 00:37:41 +0200

    Agregado CatalogoController y vista donde se muestra el catálogo y se pueden aplicar filtros

--- src/config/mapping/WebPath.java
+++ src/config/mapping/WebPath.java
@@ -12,6 +12,8 @@ public class WebPath {
 	    LOGIN_JSP("index/login.jsp"),
 	    LOGOUT_CONTROLLER("logout"),
 	    ALTA_USUARIO("index/alta_usuario.jsp"),
+	    CATALOGO_CONTROLLER("catalogo"),
+	    CATALOGO_JSP("index/catalogo.jsp"),
 	    RESULTADO("WEB-INF/resultado/index.jsp");
 
 		// Parte Dashboard (privada).
---
+++ src/main/java/curso/java/tienda/controller/catalogo/CatalogoController.java
@@ -0,0 +1,42 @@
+package curso.java.tienda.controller.catalogo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mapping.WebPath;
+
+/**
+ * Servlet implementation class CatalogoController
+ */
+@WebServlet("/catalogo")
+public class CatalogoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CatalogoController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher(WebPath.URL.CATALOGO_JSP.toString()).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
